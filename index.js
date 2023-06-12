7af6a42895210a40d3cd7f87d7fee94824b6bfc5
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-12 13:43:24 +0300

    Assignment done

---
+++ index.js
@@ -0,0 +1,46 @@
+//Addition function
+function add(a, b) {
+    let result = a + b;
+    return result;
+}
+
+//Subtraction function
+function subtract(a, b) {
+    let result = a - b;
+    return result;
+}
+
+//Multiplication Function
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+}
+
+//Division Function
+function divide(a, b) {
+    let result = a / b;
+    return result;
+}
+
+// Increment Function
+function increment(n){
+    let result = n + 1;
+    return result;
+}
+
+// Decrement Function
+function decrement(n){
+    let result = n - 1;
+    return result;
+}
+
+//Parsing an ineger
+function makeInt(n) {
+    let parsedINt = parseInt(n, 10)
+    return parsedINt;
+}
+
+// Preserve decimal function
+function preserveDecimal(n){
+    return parseFloat(n);
+}
